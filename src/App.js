9d1eab0cc8f9368f79b4bb7608d311ab5bd6f432
zongxilli <zongxi2014@gmail.com>
2021-09-23 23:09:11 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Grid, Header, Icon, Image, Input } from 'semantic-ui-react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState({ students: [] });
+	const [search, setSearch] = useState('');
+	const [dropdownIdx, setDropdownIdx] = useState(-1);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const { data } = await axios(
+				'https://api.hatchways.io/assessment/students'
+			);
+
+			setData(data);
+		};
+
+		fetchData();
+	}, [search]);
+
+	const getAverage = (array) => {
+		return (
+			array.reduce((acc, grade) => acc + parseInt(grade, 10), 0) / array.length
+		);
+	};
+
+	const dropdownIndexControl = (curr) => {
+		if (curr === dropdownIdx) setDropdownIdx(-1);
+		else setDropdownIdx(curr);
+	};
+
+	return (
+		<div
+			style={{
+				width: '60vw',
+				marginLeft: '20vw',
+				border: '1px solid lightgrey',
+			}}
+		>
+			<Input
+				fluid
+				placeholder='Search by name'
+				value={search}
+				style={{ height: '5vh' }}
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+			<Input fluid placeholder='Search by tag' style={{ height: '5vh' }} />
+
+			<div style={{ marginTop: '2vh' }}>
+				{data.students
+					.filter(
+						(student) =>
+							student.firstName.toUpperCase().includes(search.toUpperCase()) ||
+							student.lastName.toUpperCase().includes(search.toUpperCase())
+					)
+					.map((student, index) => (
+						<Grid key={student.id} floated='right'>
+							<Grid.Column width={4}>
+								<Image
+									src={student.pic}
+									circular
+									size='small'
+									style={{ marginLeft: '3vw', border: '1px solid lightgrey' }}
+								/>
+							</Grid.Column>
+							<Grid.Column width={10}>
+								<Header as='h1'>
+									{student.firstName.toUpperCase()}{' '}
+									{student.lastName.toUpperCase()}
+									<Header.Subheader>{student.email}</Header.Subheader>
+									<Header.Subheader>{student.company}</Header.Subheader>
+									<Header.Subheader>skill {student.skill}</Header.Subheader>
+									<Header.Subheader>
+										average {getAverage(student.grades)} %
+									</Header.Subheader>
+								</Header>
+								{index === dropdownIdx &&
+									student.grades.map((score, index) => (
+										<div key={index}>
+											<Grid columns={5}>
+												<Grid.Row>
+													<Grid.Column>Test {index}</Grid.Column>
+													<Grid.Column>{score} %</Grid.Column>
+												</Grid.Row>
+											</Grid>
+										</div>
+									))}
+							</Grid.Column>
+							<Grid.Column width={2}>
+								<Icon
+									name={index === dropdownIdx ? 'minus' : 'add'}
+									size='large'
+									color='grey'
+									onClick={() => dropdownIndexControl(index)}
+								/>
+							</Grid.Column>
+						</Grid>
+					))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
